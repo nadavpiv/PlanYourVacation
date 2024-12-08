dfd7a83a0698f650a5fa72e44d41335fba65c257
Nadav Pivonia <88450372+nadavpiv@users.noreply.github.com>
2024-12-08 15:55:07 +0200

    Update SwaggerConfig.java

--- vacation/src/main/java/com/nadavpiv/vacation/config/SwaggerConfig.java
+++ vacation/src/main/java/com/nadavpiv/vacation/config/SwaggerConfig.java
@@ -9,15 +9,15 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2 // Enables Swagger 2 for the Spring Boot project, which is used to generate API documentation.
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2) // Specify that we're using Swagger 2 for documentation.
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build(); // Builds the Docket instance with the selected configurations.
     }
 }
 
