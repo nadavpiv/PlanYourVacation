1fba481f01911e3b074cd6f72fb36f2a34b57522
Nadav Pivonia <88450372+nadavpiv@users.noreply.github.com>
2024-12-08 16:21:16 +0200

    Update VacationRepository.java

--- vacation/src/main/java/com/nadavpiv/vacation/repo/VacationRepository.java
+++ vacation/src/main/java/com/nadavpiv/vacation/repo/VacationRepository.java
@@ -9,10 +9,15 @@ import java.util.List;
 
 @Repository
 public interface VacationRepository extends MongoRepository<Vacation, String> {
+
+    // Custom query method to find vacation duplicates based on city, month, number of passengers, and price.
+    // The @Query annotation defines a MongoDB query that matches documents based on the parameters provided.
     @Query("{'city': ?0, 'month': ?1, 'passengers': ?2, 'price': ?3}")
     List<Vacation> findDuplicate(String city, String month, Integer passengers, Integer price);
 
-    // Custom query method to find vacations by the given criteria including price range
+    // Custom query method to find vacations by various criteria, including country, number of passengers, month, 
+    // number of days, price range, base country, and whether it's a road trip
+    // The query method name is automatically used to generate the query based on the method signature
     List<Vacation> findByCountryAndPassengersAndMonthAndNumberOfDaysBetweenAndPriceBetweenAndBaseCountryAndRoadTrip(
             String country,
             Integer passengers,
