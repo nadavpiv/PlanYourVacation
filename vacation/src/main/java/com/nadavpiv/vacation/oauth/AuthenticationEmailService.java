5b39ad5662c76b81f7996a33b9487be85f8c07df
Nadav Pivonia <88450372+nadavpiv@users.noreply.github.com>
2024-12-08 16:09:45 +0200

    Update AuthenticationEmailService.java

--- vacation/src/main/java/com/nadavpiv/vacation/oauth/AuthenticationEmailService.java
+++ vacation/src/main/java/com/nadavpiv/vacation/oauth/AuthenticationEmailService.java
@@ -9,14 +9,23 @@ import java.util.Map;
 @Service
 public class AuthenticationEmailService {
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationEmailService.class);
+
+    // Method to extract email address from the Authentication object.
     public String extractEmailFromAuthentication(Authentication authentication) {
         logger.info("Extract the email that the user login with for sending mail");
+
+        // Check if authentication is not null.
         if (authentication != null) {
+            // Get the principal (user details) from the authentication object.
             Object principal = authentication.getPrincipal();
+
+             // Check if the principal is of type OAuth2User (meaning OAuth2 login was used).
             if (principal instanceof OAuth2User) {
-                OAuth2User oAuth2User = (OAuth2User) principal;
-                Map<String, Object> attributes = oAuth2User.getAttributes();
-                return (String) attributes.get("email"); // Adjust the key if needed
+                OAuth2User oAuth2User = (OAuth2User) principal; // Cast the principal to OAuth2User.
+                Map<String, Object> attributes = oAuth2User.getAttributes(); // Get the user attributes from OAuth2User.
+
+                // Extract the email attribute from the user attributes map.
+                return (String) attributes.get("email"); // Return the email associated with the user.
             }
         }
         return null;
